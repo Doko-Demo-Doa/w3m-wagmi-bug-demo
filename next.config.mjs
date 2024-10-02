0d3df8e17faea9c0a4e06760c643a65225c9163c
Quan Pham <doraemonfanclub@gmail.com>
2024-10-02 20:39:51 +0700

    Broader tracing

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     outputFileTracingIncludes: {
-      "/crash": ["./node_modules/@reown/appkit/dist/esm/src/client.js"],
+      "/crash": ["./node_modules/@reown/"],
     },
   },
 };
